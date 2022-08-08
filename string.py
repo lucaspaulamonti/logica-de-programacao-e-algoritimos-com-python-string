251ce18fb979d81fee873d901568312a473eb3f3
Lucas Monti <108158100+MontiLucas@users.noreply.github.com>
2022-08-07 22:02:17 -0300

    Initial commit

---
+++ string.py
@@ -0,0 +1,7 @@
+# Utilizando operadores + e * com strings.
+s1,s2,s3='ant','bat','cod'
+print(s1,s2,s3)
+print((s1+' ')*10)
+print(s1,(s2+' ')*2+(s3+' ')*3)
+print(((s1+' ')+(s2+' '))*7)
+print((s2+s2+s3+' ')*5)
